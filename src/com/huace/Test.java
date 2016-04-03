cb3c15961419d85100010c9e00d76415a59e3d26
zhangjiadao <zhangjiadao@huacemedia.com>
2016-04-03 17:44:47 +0800

    测试

---
+++ src/com/huace/Test.java
@@ -0,0 +1,27 @@
+package com.huace;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+public class Test
+{
+	public static void main(String[] args)
+	{
+		BasicCookieStore store = new BasicCookieStore();
+	}
+}
